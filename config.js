3f240c376529bfc4b212355be4eb4371a5ed97fa
devms786178-lgtm <devms786178@gmail.com>
2026-08-11 17:33:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ require("dotenv").config(); // Optional: if using a .env file
 
 const config = {
   BOT_TOKEN: process.env.BOT_TOKEN || " ",
-  MONGO_URI: process.env.MONGO_URI || " ",
-  OWNER_ID: Number(process.env.OWNER_ID || "6713397633"),
+  MONGO_URI: process.env.MONGO_URI || "mongodb+srv://developerbro723_db_user:9axC7c7iQm0G3ESO@cluster0.dr8m75m.mongodb.net/?appName=Cluster0",
+  OWNER_ID: Number(process.env.OWNER_ID || "8909902924"),
   START_IMAGE_URL: process.env.START_IMAGE_URL || " ",
 };
 
