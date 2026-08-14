0ad41f1a6e73eba10436c2c5a24b662a851775c8
sigmacat786-sudo <sigmacat786@gmail.com>
2026-08-14 11:49:08 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -169,7 +169,7 @@ bot
         });
 
         await singleFile.save();
-        const shareLink = `https://t.me/Nawaab_FileStoreBot?start=${uniqueId}`;
+        const shareLink = `https://t.me/PWSENSEI_FileStoreBot?start=${uniqueId}`;
         bot.sendMessage(
           msg.chat.id,
           `File saved! Shareable link: ${shareLink}`
@@ -209,7 +209,7 @@ bot
         });
 
         await singleFile.save();
-        const shareLink = `https://t.me/Nawaab_FileStoreBot?start=${uniqueId}`;
+        const shareLink = `https://t.me/PWSENSEI_FileStoreBot?start=${uniqueId}`;
         bot.sendMessage(
           msg.chat.id,
           `File saved! Shareable link: ${shareLink}`
@@ -246,7 +246,7 @@ bot
         });
 
         await singleFile.save();
-        const shareLink = `https://t.me/Nawaab_FileStoreBot?start=${uniqueId}`;
+        const shareLink = `https://t.me/PWSENSEI_FileStoreBot?start=${uniqueId}`;
         bot.sendMessage(
           msg.chat.id,
           `Video saved! Shareable link: ${shareLink}`
@@ -283,7 +283,7 @@ bot
         });
 
         await singleFile.save();
-        const shareLink = `https://t.me/Nawaab_FileStoreBot?start=${uniqueId}`;
+        const shareLink = `https://t.me/PWSENSEI_FileStoreBot?start=${uniqueId}`;
         bot.sendMessage(
           msg.chat.id,
           `Audio saved! Shareable link: ${shareLink}`
@@ -317,7 +317,7 @@ bot
         await batchData.save();
 
         // Generate the shareable link
-        const shareLink = `https://t.me/Nawaab_FileStoreBot?start=${currentBatchId}`;
+        const shareLink = `https://t.me/PWSENSEI_FileStoreBot?start=${currentBatchId}`;
         bot.sendMessage(
           msg.chat.id,
           `Batch saved successfully! Shareable link: ${shareLink}`
