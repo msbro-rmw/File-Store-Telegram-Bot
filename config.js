67c3eadf320697d782ae6d93fb3e9d8441bc444e
msbro-rmw <mdinabano189@gmail.com>
2026-08-18 10:37:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require("dotenv").config(); // Optional: if using a .env file
 
 const config = {
-  BOT_TOKEN: process.env.BOT_TOKEN || "8556508365:AAH4GSG4PVv_4lxw1QqDpr--suu1zQruxKA",
-  MONGO_URI: process.env.MONGO_URI || "mongodb+srv://developerbro723_db_user:9axC7c7iQm0G3ESO@cluster0.dr8m75m.mongodb.net/?appName=Cluster0",
+  BOT_TOKEN: process.env.BOT_TOKEN || "8556508365:AAFIepFJ0bFekHLuYNT-6vjzfL1_lLqvzLM",
+  MONGO_URI: process.env.MONGO_URI || "mongodb+srv://devms786178_db_user:cEtMdLjmHF5EM2Pf@cluster0.xbqyvnn.mongodb.net/?appName=Cluster0",
   OWNER_ID: Number(process.env.OWNER_ID || "8909902924"),
   START_IMAGE_URL: process.env.START_IMAGE_URL || "https://graph.org/file/dabc3b293f0ab07a49eab-f3d1061ff5994e7b50.jpg",
 };
